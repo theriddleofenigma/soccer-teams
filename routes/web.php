3ef0b00e7807171a7b2797ca61383e280c75b3ee
Kumaravel <kumaravel@bepro.today>
2023-05-06 21:19:07 +0530

    Updated the home route with success response instead of rendering the welcome view.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Response::json([
+        'message' => 'success',
+    ]);
 });
